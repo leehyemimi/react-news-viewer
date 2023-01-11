d1fb3caca983ad36ecbd9c9a73b33e3ddd58eaef
이혜원 <sunone222@gmail.com>
2023-01-11 17:45:10 +0900

    api연결해서 data까지 넣음

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import './App.css';
-import axios from 'axios'
-import { useState } from "react";
-function App() {
-	const [data, setData] = useState([]);
-	function onClick() {
-		axios.get('https://jsonplaceholder.typicode.com/todos/1').then((Response) => {
-			setData(Response.data);
-		}).catch((Error) => {
-			console.log(Error);
-		})
-	}
 
+import NewsList from "./components/NewsList";
+function App() {
 	return (
 		<div className="App">
-			<button onClick={onClick}>불러오기</button>
-			<div>
-				{data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
-			</div>
+			<NewsList></NewsList>
 		</div>
 	);
 }
