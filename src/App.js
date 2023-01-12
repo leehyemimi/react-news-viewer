1abce279d66bfafccf9a74fd560cbb238ac8163b
테스트 <sunone222@gmail.com>
2023-01-12 23:50:46 +0900

    카테고리컴포넌트추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Categories from "./components/Categories";
 
 import NewsList from "./components/NewsList";
 function App() {
 	return (
 		<div className="App">
+			<Categories></Categories>
 			<NewsList></NewsList>
 		</div>
 	);
